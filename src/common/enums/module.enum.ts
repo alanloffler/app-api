681333a16da0cd088a55936d6e3a3fef1a77722c
alanloffler <alanmatiasloffler@gmail.com>
2026-01-05 14:07:12 -0300

    refactor(enums): module enum as const

--- src/common/enums/module.enum.ts
+++ src/common/enums/module.enum.ts
@@ -1,5 +1,7 @@
-export enum EModule {
-  APP = "app",
-  DASHBOARD = "dashboard",
-  NOTIFICATION = "notification",
-}
+export const EModule = {
+  APP: "app",
+  DASHBOARD: "dashboard",
+  NOTIFICATION: "notification",
+} as const;
+
+export type EModule = (typeof EModule)[keyof typeof EModule];

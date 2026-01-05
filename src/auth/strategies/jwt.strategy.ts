7bd19708853ce90c369f5cbca326d6dcc1a55054
alanloffler <alanmatiasloffler@gmail.com>
2026-01-05 18:52:10 -0300

    feat(auth): validate by user type

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -5,13 +5,19 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 import type { IPayload } from "@auth/interfaces/payload.interface";
+import { Admin } from "@/admin/entities/admin.entity";
 import { AdminService } from "@admin/admin.service";
+import { ApiResponse } from "@/common/helpers/api-response.helper";
+import { EAuthType } from "@auth/enums/auth-type.enum";
+import { User } from "@/users/entities/user.entity";
+import { UsersService } from "@users/users.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     configService: ConfigService,
     private readonly adminService: AdminService,
+    private readonly usersService: UsersService,
   ) {
     const secret = configService.get<string>("JWT_SECRET");
     if (!secret) throw new Error("JWT_SECRET no está definido");
@@ -29,14 +35,24 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: IPayload) {
-    const admin = await this.adminService.findOne(payload.id);
-    if (!admin) throw new HttpException("Admin no encontrado", HttpStatus.UNAUTHORIZED);
+    let user: ApiResponse<Admin | User> | null = null;
+
+    if (payload.type === EAuthType.USER) {
+      user = await this.usersService.findOne(payload.id);
+      if (!user) throw new HttpException("Usuario no encontrado", HttpStatus.UNAUTHORIZED);
+    } else if (payload.type === EAuthType.ADMIN) {
+      user = await this.adminService.findOne(payload.id);
+      if (!user) throw new HttpException("Admin no encontrado", HttpStatus.UNAUTHORIZED);
+    } else {
+      throw new HttpException("Tipo de usuario inválido", HttpStatus.UNAUTHORIZED);
+    }
 
     return {
-      id: admin.data?.id,
-      email: admin.data?.email,
-      role: admin.data?.role.value,
-      roleId: admin.data?.role.id,
+      id: user.data?.id,
+      email: user.data?.email,
+      role: user.data?.role.value,
+      roleId: user.data?.role.id,
+      type: payload.type,
     };
   }
 }

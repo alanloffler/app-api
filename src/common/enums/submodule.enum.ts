deb8e1c84e8b2957b692495feae01d781dbcb0aa
alanloffler <alanmatiasloffler@gmail.com>
2026-01-05 14:07:36 -0300

    refactor(enums): submodule enum as const

--- src/common/enums/submodule.enum.ts
+++ src/common/enums/submodule.enum.ts
@@ -1,5 +1,7 @@
-export enum ESubmodule {
-  EMAIL = "email",
-  MENU = "menu",
-  THEME = "theme",
-}
+export const ESubmodule = {
+  EMAIL: "email",
+  MENU: "menu",
+  THEME: "theme",
+} as const;
+
+export type ESubmodule = (typeof ESubmodule)[keyof typeof ESubmodule];

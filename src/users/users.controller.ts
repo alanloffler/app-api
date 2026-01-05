c1948d6048a62283b51f507b8a6729ac4ec90d32
alanloffler <alanmatiasloffler@gmail.com>
2026-01-04 21:49:31 -0300

    feat(users): controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
+
+import { CreateUserDto } from "@users/dto/create-user.dto";
+import { JwtAuthGuard } from "@auth/guards/jwt-auth.guard";
+import { PermissionsGuard } from "@auth/guards/permissions.guard";
+import { RequiredPermissions } from "@auth/decorators/required-permissions.decorator";
+import { UpdateUserDto } from "@users/dto/update-user.dto";
+import { UsersService } from "@users/users.service";
+
+@UseGuards(JwtAuthGuard, PermissionsGuard)
+@Controller("users")
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  // @RequiredPermissions("users-create")
+  @Post()
+  create(@Body() user: CreateUserDto) {
+    return this.usersService.create(user);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.usersService.remove(+id);
+  }
+}

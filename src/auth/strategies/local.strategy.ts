e60c6281d0b103ec2dbf1b88168808f9701e0923
alanloffler <alanmatiasloffler@gmail.com>
2026-01-04 23:12:11 -0300

    feat(auth): find user by type

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,22 +1,40 @@
 import * as bcrypt from "bcrypt";
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
+import { Request } from "express";
 import { Strategy } from "passport-local";
 
 import type { IPayload } from "@auth/interfaces/payload.interface";
+import { Admin } from "@admin/entities/admin.entity";
 import { AdminService } from "@admin/admin.service";
+import { User } from "@users/entities/user.entity";
+import { UsersService } from "@users/users.service";
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly adminService: AdminService) {
+  constructor(
+    private readonly adminService: AdminService,
+    private readonly userService: UsersService,
+  ) {
     super({
       usernameField: "email",
       passwordField: "password",
+      passReqToCallback: true,
     });
   }
 
-  async validate(email: string, password: string): Promise<IPayload> {
-    const user = await this.adminService.findOneByEmail(email);
+  async validate(req: Request, email: string, password: string): Promise<IPayload> {
+    const type = req.body.type;
+
+    let user: Admin | User | null = null;
+
+    if (type === "user") {
+      user = await this.userService.findOneByEmail(email);
+    } else if (type === "admin") {
+      user = await this.adminService.findOneByEmail(email);
+    } else {
+      throw new HttpException("Credenciales inválidas (type)", HttpStatus.UNAUTHORIZED);
+    }
 
     if (!user) throw new HttpException("Credenciales inválidas", HttpStatus.UNAUTHORIZED);
 

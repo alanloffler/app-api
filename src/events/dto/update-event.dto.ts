43c72fba78a87978836df35c851ad32cb3c1ae41
alanloffler <alanmatiasloffler@gmail.com>
2026-01-10 14:43:12 -0300

    feat(events): update event dto

---
+++ src/events/dto/update-event.dto.ts
@@ -0,0 +1,5 @@
+import { PartialType } from "@nestjs/mapped-types";
+
+import { CreateEventDto } from "@events/dto/create-event.dto";
+
+export class UpdateEventDto extends PartialType(CreateEventDto) {}

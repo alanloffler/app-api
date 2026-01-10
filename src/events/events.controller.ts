0e5d0965995fd493eca68ba7e498b5f8c135c24b
alanloffler <alanmatiasloffler@gmail.com>
2026-01-10 18:41:07 -0300

    feat(events): controller permissions

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -4,6 +4,7 @@ import { CreateEventDto } from "@events/dto/create-event.dto";
 import { EventsService } from "@events/events.service";
 import { JwtAuthGuard } from "@auth/guards/jwt-auth.guard";
 import { PermissionsGuard } from "@auth/guards/permissions.guard";
+import { RequiredPermissions } from "@auth/decorators/required-permissions.decorator";
 import { UpdateEventDto } from "@events/dto/update-event.dto";
 
 @UseGuards(JwtAuthGuard, PermissionsGuard)
@@ -11,6 +12,7 @@ import { UpdateEventDto } from "@events/dto/update-event.dto";
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
+  @RequiredPermissions("events-create")
   @Post()
   create(@Body() createEventDto: CreateEventDto) {
     return this.eventsService.create(createEventDto);

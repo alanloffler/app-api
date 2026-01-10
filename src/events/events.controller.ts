3abe229591499115e51d21f02ad8571421570a09
alanloffler <alanmatiasloffler@gmail.com>
2026-01-10 18:36:36 -0300

    feat(events): auth guards

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
 
 import { CreateEventDto } from "@events/dto/create-event.dto";
 import { EventsService } from "@events/events.service";
+import { JwtAuthGuard } from "@auth/guards/jwt-auth.guard";
+import { PermissionsGuard } from "@auth/guards/permissions.guard";
 import { UpdateEventDto } from "@events/dto/update-event.dto";
 
+@UseGuards(JwtAuthGuard, PermissionsGuard)
 @Controller("events")
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
@@ -20,16 +23,16 @@ export class EventsController {
 
   @Get(":id")
   findOne(@Param("id") id: string) {
-    return this.eventsService.findOne(+id);
+    return this.eventsService.findOne(id);
   }
 
   @Patch(":id")
   update(@Param("id") id: string, @Body() updateEventDto: UpdateEventDto) {
-    return this.eventsService.update(+id, updateEventDto);
+    return this.eventsService.update(id, updateEventDto);
   }
 
   @Delete(":id")
   remove(@Param("id") id: string) {
-    return this.eventsService.remove(+id);
+    return this.eventsService.remove(id);
   }
 }

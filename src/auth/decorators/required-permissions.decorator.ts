05920f5864cd96f20ea59ff0d25d8ff4b300d74f
alanloffler <alanmatiasloffler@gmail.com>
2026-01-14 20:54:17 -0300

    feat(auth): handle permissions[] with mode

--- src/auth/decorators/required-permissions.decorator.ts
+++ src/auth/decorators/required-permissions.decorator.ts
@@ -2,4 +2,7 @@ import { SetMetadata } from "@nestjs/common";
 
 export const PERMISSIONS_KEY: string = "permissions";
 
-export const RequiredPermissions = (...permissions: string[]) => SetMetadata(PERMISSIONS_KEY, permissions);
+export const RequiredPermissions = (permissions: string | string[], mode: "some" | "every" = "every") => {
+  const perms = Array.isArray(permissions) ? permissions : [permissions];
+  return SetMetadata(PERMISSIONS_KEY, { permissions: perms, mode });
+};

12b660c4d2aae379115ada12dbf1183d041290c5
alanloffler <alanmatiasloffler@gmail.com>
2026-01-15 13:13:27 -0300

    feat(users): complete permissions & rename controllers path

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,81 +12,90 @@ import { UsersService } from "@users/users.service";
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @RequiredPermissions(["patient-create", "professional-create"], "some")
+  @RequiredPermissions(["admin-create", "patient-create", "professional-create"], "some")
   @Post()
   create(@Body() user: CreateUserDto) {
     return this.usersService.create(user);
   }
 
-  @RequiredPermissions(["patient-view", "professional-view"], "some")
-  @Get(":role")
+  @RequiredPermissions(
+    ["admin-create", "admin-update", "patient-create", "patient-update", "professional-create", "professional-update"],
+    "some",
+  )
+  @Get("check/email/:email")
+  checkEmailAvailability(@Param("email") email: string) {
+    return this.usersService.checkEmailAvailability(email);
+  }
+
+  @RequiredPermissions(
+    ["admin-create", "admin-update", "patient-create", "patient-update", "professional-create", "professional-update"],
+    "some",
+  )
+  @Get("check/ic/:ic")
+  checkIcAvailability(@Param("ic") id: string) {
+    return this.usersService.checkIcAvailability(id);
+  }
+
+  @RequiredPermissions(
+    ["admin-create", "admin-update", "patient-create", "patient-update", "professional-create", "professional-update"],
+    "some",
+  )
+  @Get("check/username/:username")
+  checkUsernameAvailability(@Param("username") userName: string) {
+    return this.usersService.checkUsernameAvailability(userName);
+  }
+
+  @RequiredPermissions(["admin-view", "patient-view", "professional-view"], "some")
+  @Get("role/:role")
   findAll(@Param("role") role: string) {
     return this.usersService.findAll(role);
   }
 
-  @RequiredPermissions(["patient-view", "professional-view"], "some")
-  @Get("soft-removed/:role")
+  @RequiredPermissions(["admin-view", "patient-view", "professional-view"], "some")
+  @Get("all-soft-remove/:role")
   findAllSoftRemoved(@Param("role") role: string) {
     return this.usersService.findAllSoftRemoved(role);
   }
 
-  @RequiredPermissions(["patient-view", "professional-view"], "some")
-  @Get(":id/credentials")
+  @RequiredPermissions(["admin-view", "patient-view", "professional-view"], "some")
+  @Get("soft-remove/:id")
+  findOneSoftRemoved(@Param("id", ParseUUIDPipe) id: string) {
+    return this.usersService.findOneSoftRemoved(id);
+  }
+
+  @RequiredPermissions(["admin-view", "patient-view", "professional-view"], "some")
+  @Get("credential/:id")
   findOneWithCredentials(@Param("id", ParseUUIDPipe) id: string) {
     return this.usersService.findOneWithCredentials(id);
   }
 
-  @RequiredPermissions(["patient-view", "professional-view"], "some")
+  @RequiredPermissions(["admin-view", "patient-view", "professional-view"], "some")
   @Get(":id")
   findOne(@Param("id", ParseUUIDPipe) id: string) {
     return this.usersService.findOne(id);
   }
 
-  @RequiredPermissions(["patient-view", "professional-view"], "some")
-  @Get(":id/soft-removed")
-  findOneSoftRemoved(@Param("id", ParseUUIDPipe) id: string) {
-    return this.usersService.findOneSoftRemoved(id);
-  }
-
-  @RequiredPermissions(["patient-update", "professional-update"], "some")
+  @RequiredPermissions(["admin-update", "patient-update", "professional-update"], "some")
   @Patch(":id")
   update(@Param("id", ParseUUIDPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
-  @RequiredPermissions(["patient-delete", "professional-delete"], "some")
-  @Delete("soft-remove/:id")
-  softRemove(@Param("id", ParseUUIDPipe) id: string) {
-    return this.usersService.softRemove(id);
-  }
-
-  @RequiredPermissions(["patient-delete-hard", "professional-delete-hard"], "some")
-  @Delete(":id")
-  remove(@Param("id", ParseUUIDPipe) id: string) {
-    return this.usersService.remove(id);
-  }
-
-  @RequiredPermissions(["patient-restore", "professional-restore"], "some")
+  @RequiredPermissions(["admin-restore", "patient-restore", "professional-restore"], "some")
   @Patch("restore/:id")
   restore(@Param("id", ParseUUIDPipe) id: string) {
     return this.usersService.restore(id);
   }
 
-  // TODO: manage multi permissions (here: admin-create, admin-update)
-  @Get("/email-availability/:email")
-  checkEmailAvailability(@Param("email") email: string) {
-    return this.usersService.checkEmailAvailability(email);
+  @RequiredPermissions(["admin-delete", "patient-delete", "professional-delete"], "some")
+  @Delete("soft-remove/:id")
+  softRemove(@Param("id", ParseUUIDPipe) id: string) {
+    return this.usersService.softRemove(id);
   }
 
-  // TODO: manage multi permissions (here: admin-create, admin-update)
-  @Get("/ic-availability/:ic")
-  checkIcAvailability(@Param("ic") id: string) {
-    return this.usersService.checkIcAvailability(id);
-  }
-
-  // TODO: manage multi permissions (here: admin-create, admin-update)
-  @Get("/username-availability/:userName")
-  checkUsernameAvailability(@Param("userName") userName: string) {
-    return this.usersService.checkUsernameAvailability(userName);
+  @RequiredPermissions(["admin-delete-hard", "patient-delete-hard", "professional-delete-hard"], "some")
+  @Delete(":id")
+  remove(@Param("id", ParseUUIDPipe) id: string) {
+    return this.usersService.remove(id);
   }
 }

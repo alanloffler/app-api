74470d94f3afec22cad8ed19c17b9bab40b61faa
alanloffler <alanmatiasloffler@gmail.com>
2026-01-16 21:48:18 -0300

    feat(business): auth & permissons guard

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
 
 import { BusinessService } from "@business/business.service";
 import { CreateBusinessDto } from "@business/dto/create-business.dto";
+import { JwtAuthGuard } from "@auth/guards/jwt-auth.guard";
+import { PermissionsGuard } from "@auth/guards/permissions.guard";
 import { UpdateBusinessDto } from "@business/dto/update-business.dto";
 
+// TODO: add permissions
+@UseGuards(JwtAuthGuard, PermissionsGuard)
 @Controller("business")
 export class BusinessController {
   constructor(private readonly businessService: BusinessService) {}

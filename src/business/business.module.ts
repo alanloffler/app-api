10d03258e6a1432cdfb1738897de2dbf12512d0d
alanloffler <alanmatiasloffler@gmail.com>
2026-01-16 21:49:52 -0300

    feat(business): import entities for guards

--- src/business/business.module.ts
+++ src/business/business.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Business } from "@business/entities/business.entity";
 import { BusinessController } from "@business/business.controller";
 import { BusinessService } from "@business/business.service";
+import { Permission } from "@permissions/entities/permission.entity";
+import { Role } from "@roles/entities/role.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Business])],
+  imports: [TypeOrmModule.forFeature([Business, Permission, Role])],
   controllers: [BusinessController],
   providers: [BusinessService],
 })

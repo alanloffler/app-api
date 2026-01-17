2fbe7f5f2dd96a7465d593ece015c1f2a25b6f25
alanloffler <alanmatiasloffler@gmail.com>
2026-01-16 21:46:05 -0300

    feat(business): business entity import

--- src/business/business.module.ts
+++ src/business/business.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { Business } from "@business/entities/business.entity";
 import { BusinessController } from "@business/business.controller";
 import { BusinessService } from "@business/business.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Business])],
   controllers: [BusinessController],
   providers: [BusinessService],
 })

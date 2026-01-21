dcac37fb2e7a9df5f9e7f32587177bc577b88da8
alanloffler <alanmatiasloffler@gmail.com>
2026-01-21 17:28:04 -0300

    feat(professional-profile): update entity structure

--- src/professional-profile/entities/professional-profile.entity.ts
+++ src/professional-profile/entities/professional-profile.entity.ts
@@ -1,1 +1,39 @@
-export class ProfessionalProfile {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+import { User } from "@users/entities/user.entity";
+
+@Entity()
+export class ProfessionalProfile {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "uuid", name: "user_id", unique: true })
+  userId: string;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @Column({ type: "varchar", length: 50, name: "license_id", nullable: false })
+  licenseId: string;
+
+  @Column({ type: "simple-array", nullable: false })
+  specialties: string[];
+
+  @Column({ type: "simple-array", name: "working_days", nullable: false })
+  workingDays: string[];
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
+}

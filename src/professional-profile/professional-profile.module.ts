9eeda0d7735afba1f6067d0411dbd5c1b2f6db8f
alanloffler <alanmatiasloffler@gmail.com>
2026-01-21 18:21:09 -0300

    feat(professional-profile): import typeorm module

--- src/professional-profile/professional-profile.module.ts
+++ src/professional-profile/professional-profile.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { ProfessionalProfile } from "@professional-profile/entities/professional-profile.entity";
 import { ProfessionalProfileController } from "@professional-profile/professional-profile.controller";
 import { ProfessionalProfileService } from "@professional-profile/professional-profile.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProfessionalProfile])],
   controllers: [ProfessionalProfileController],
   providers: [ProfessionalProfileService],
 })

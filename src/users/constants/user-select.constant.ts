b8dbcac166725631b420d1c2b61a9711ab7ee944
alanloffler <alanmatiasloffler@gmail.com>
2026-01-22 21:54:05 -0300

    feat(users): user select constants (user, profile & role)

---
+++ src/users/constants/user-select.constant.ts
@@ -0,0 +1,25 @@
+export const USER_SELECT = [
+  "user.id",
+  "user.ic",
+  "user.userName",
+  "user.firstName",
+  "user.lastName",
+  "user.email",
+  "user.phoneNumber",
+  "user.role",
+  "user.createdAt",
+  "user.updatedAt",
+  "user.deletedAt",
+];
+
+export const USER_PROFILE_SELECT = [
+  "profile.id",
+  "profile.licenseId",
+  "profile.professionalPrefix",
+  "profile.specialty",
+  "profile.workingDays",
+  "profile.createdAt",
+  "profile.updatedAt",
+];
+
+export const USER_ROLE_SELECT = ["role.id", "role.name", "role.value", "role.description"];

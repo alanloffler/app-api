2db6b52c6355bc988f3fab0212ba5af4cd0d94db
alanloffler <alanmatiasloffler@gmail.com>
2026-01-25 17:23:29 -0300

    remove(admin): remove entity, admin are an user role

--- src/admin/dto/update-admin.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-
-import { CreateAdminDto } from "@admin/dto/create-admin.dto";
-
-export class UpdateAdminDto extends PartialType(CreateAdminDto) {}

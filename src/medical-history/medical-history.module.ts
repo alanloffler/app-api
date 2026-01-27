4012758ef0e01a93f01d426f1a5bee424a9ab746
alanloffler <alanmatiasloffler@gmail.com>
2026-01-27 20:10:29 -0300

    feat(medical-history): import typeorm module

--- src/medical-history/medical-history.module.ts
+++ src/medical-history/medical-history.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { MedicalHistory } from "@medical-history/entities/medical-history.entity";
 import { MedicalHistoryController } from "@medical-history/medical-history.controller";
 import { MedicalHistoryService } from "@medical-history/medical-history.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MedicalHistory])],
   controllers: [MedicalHistoryController],
   providers: [MedicalHistoryService],
 })

8e70adbaa0470abb579a06caec8a76d3d14e852e
alanloffler <alanmatiasloffler@gmail.com>
2026-01-27 19:44:25 -0300

    feat(medical-history): controller

---
+++ src/medical-history/medical-history.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+
+import { CreateMedicalHistoryDto } from "@medical-history/dto/create-medical-history.dto";
+import { MedicalHistoryService } from "@medical-history/medical-history.service";
+import { UpdateMedicalHistoryDto } from "@medical-history/dto/update-medical-history.dto";
+
+@Controller("medical-history")
+export class MedicalHistoryController {
+  constructor(private readonly medicalHistoryService: MedicalHistoryService) {}
+
+  @Post()
+  create(@Body() createMedicalHistoryDto: CreateMedicalHistoryDto) {
+    return this.medicalHistoryService.create(createMedicalHistoryDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.medicalHistoryService.findAll();
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.medicalHistoryService.findOne(id);
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateMedicalHistoryDto: UpdateMedicalHistoryDto) {
+    return this.medicalHistoryService.update(id, updateMedicalHistoryDto);
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.medicalHistoryService.remove(id);
+  }
+}

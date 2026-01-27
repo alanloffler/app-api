b0c526305d833c8f54f069da69879b859ef529f6
alanloffler <alanmatiasloffler@gmail.com>
2026-01-27 20:29:53 -0300

    feat(medical-history): create history service

--- src/medical-history/medical-history.service.ts
+++ src/medical-history/medical-history.service.ts
@@ -1,12 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
 
+import { ApiResponse } from "@common/helpers/api-response.helper";
 import { CreateMedicalHistoryDto } from "@medical-history/dto/create-medical-history.dto";
+import { MedicalHistory } from "@medical-history/entities/medical-history.entity";
 import { UpdateMedicalHistoryDto } from "@medical-history/dto/update-medical-history.dto";
 
 @Injectable()
 export class MedicalHistoryService {
-  create(createMedicalHistoryDto: CreateMedicalHistoryDto) {
-    return "This action adds a new medicalHistory";
+  constructor(
+    @InjectRepository(MedicalHistory) private readonly medicalHistoryRepository: Repository<MedicalHistory>,
+  ) {}
+
+  async create(createMedicalHistoryDto: CreateMedicalHistoryDto): Promise<ApiResponse<MedicalHistory>> {
+    // TODO:
+    // 1. Check userId existence
+    // 2. Check eventId existence
+    const history = this.medicalHistoryRepository.create(createMedicalHistoryDto);
+    const saveHistory = await this.medicalHistoryRepository.save(history);
+    if (!saveHistory) throw new HttpException("Error al crear el historial", HttpStatus.BAD_REQUEST);
+
+    return ApiResponse.created<MedicalHistory>("Historial creado", history);
   }
 
   findAll() {

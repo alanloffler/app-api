c5999cb57d4d56544b6a24487552a082079a6c93
alanloffler <alanmatiasloffler@gmail.com>
2026-01-27 20:42:15 -0300

    feat(medical-history): businessId on create controller

--- src/medical-history/medical-history.controller.ts
+++ src/medical-history/medical-history.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from "@nestjs/common";
 
+import { BusinessId } from "@common/decorators/business-id.decorator";
 import { CreateMedicalHistoryDto } from "@medical-history/dto/create-medical-history.dto";
 import { MedicalHistoryService } from "@medical-history/medical-history.service";
 import { UpdateMedicalHistoryDto } from "@medical-history/dto/update-medical-history.dto";
 
+// TODO: auth & permissions
 @Controller("medical-history")
 export class MedicalHistoryController {
   constructor(private readonly medicalHistoryService: MedicalHistoryService) {}
 
   @Post()
-  create(@Body() createMedicalHistoryDto: CreateMedicalHistoryDto) {
-    return this.medicalHistoryService.create(createMedicalHistoryDto);
+  create(@BusinessId(ParseUUIDPipe) businessId: string, @Body() createMedicalHistoryDto: CreateMedicalHistoryDto) {
+    return this.medicalHistoryService.create(businessId, createMedicalHistoryDto);
   }
 
   @Get()

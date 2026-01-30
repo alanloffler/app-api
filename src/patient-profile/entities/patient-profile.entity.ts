eb3b5fd9b522a36e7e31eebe270d6b834d5a7f4c
alanloffler <alanmatiasloffler@gmail.com>
2026-01-30 19:03:35 -0300

    feat(patient-profile): indexes

--- src/patient-profile/entities/patient-profile.entity.ts
+++ src/patient-profile/entities/patient-profile.entity.ts
@@ -2,6 +2,7 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  Index,
   JoinColumn,
   OneToOne,
   PrimaryGeneratedColumn,
@@ -11,11 +12,17 @@ import {
 import { User } from "@users/entities/user.entity";
 
 @Entity()
+@Index("idx_patient_profile_business", ["businessId"])
+@Index("idx_patient_profile_business_user", ["businessId", "userId"])
+@Index("uq_patient_profile_business_user", ["businessId", "userId"], { unique: true })
 export class PatientProfile {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
-  @Column({ type: "uuid", name: "user_id", nullable: false, unique: true })
+  @Column({ type: "uuid", name: "business_id", nullable: false })
+  businessId: string;
+
+  @Column({ type: "uuid", name: "user_id", nullable: false })
   userId: string;
 
   @OneToOne(() => User)

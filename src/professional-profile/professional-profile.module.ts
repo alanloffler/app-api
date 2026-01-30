c91e259afbc76e524d7557ba24d073b86d8e3455
alanloffler <alanmatiasloffler@gmail.com>
2026-01-30 19:11:49 -0300

    feat(professional-profile): import Role & User entities
    Needed for auth & permission guards

--- src/professional-profile/professional-profile.module.ts
+++ src/professional-profile/professional-profile.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ProfessionalProfile } from "@professional-profile/entities/professional-profile.entity";
 import { ProfessionalProfileController } from "@professional-profile/professional-profile.controller";
 import { ProfessionalProfileService } from "@professional-profile/professional-profile.service";
+import { Role } from "@roles/entities/role.entity";
+import { User } from "@users/entities/user.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProfessionalProfile])],
+  imports: [TypeOrmModule.forFeature([ProfessionalProfile, Role, User])],
   controllers: [ProfessionalProfileController],
   providers: [ProfessionalProfileService],
 })

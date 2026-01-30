b9f8e7e977027ec9a5078a2a331bbeca27e922b9
alanloffler <alanmatiasloffler@gmail.com>
2026-01-30 18:34:47 -0300

    feat(patient-profile): entity cols

--- src/patient-profile/entities/patient-profile.entity.ts
+++ src/patient-profile/entities/patient-profile.entity.ts
@@ -1,1 +1,51 @@
-export class PatientProfile {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+import { User } from "@users/entities/user.entity";
+
+@Entity()
+export class PatientProfile {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "uuid", name: "user_id", nullable: false, unique: true })
+  userId: string;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @Column({ nullable: false })
+  gender: string;
+
+  @Column({ name: "birth_day", type: "date", nullable: false })
+  birthDay: Date;
+
+  @Column({ name: "blood_type", nullable: false })
+  bloodType: string;
+
+  @Column({ nullable: false })
+  weight: number;
+
+  @Column({ nullable: false })
+  height: number;
+
+  @Column({ name: "emergency_contact_name", nullable: false })
+  emergencyContactName: string;
+
+  @Column({ name: "emergency_contact_phone", nullable: false })
+  emergencyContactPhone: string;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
+}

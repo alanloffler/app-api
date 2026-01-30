9de2d76205b5b248991d2b31901724d949541447
alanloffler <alanmatiasloffler@gmail.com>
2026-01-30 18:38:45 -0300

    feat(patient-profile): import typeorm module

--- src/patient-profile/patient-profile.module.ts
+++ src/patient-profile/patient-profile.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { PatientProfile } from "@patient-profile/entities/patient-profile.entity";
 import { PatientProfileController } from "@patient-profile/patient-profile.controller";
 import { PatientProfileService } from "@patient-profile/patient-profile.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PatientProfile])],
   controllers: [PatientProfileController],
   providers: [PatientProfileService],
 })

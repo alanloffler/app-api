5234619e3f179286234295fafc0529d5c54082cb
alanloffler <alanmatiasloffler@gmail.com>
2026-01-31 17:26:22 -0300

    feat(users): import professional use case & profile service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { CreateProfessionalUseCase } from "@users/create-professional.use-case";
+import { ProfessionalProfileService } from "@professional-profile/professional-profile.service";
 import { Role } from "@roles/entities/role.entity";
 import { User } from "@users/entities/user.entity";
 import { UsersController } from "@users/users.controller";
@@ -9,7 +11,7 @@ import { UsersService } from "@users/users.service";
 @Module({
   imports: [TypeOrmModule.forFeature([Role, User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [CreateProfessionalUseCase, ProfessionalProfileService, UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}

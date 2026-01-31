cf6287e3fc17ded36f9b08b03304e09cb4fabfcb
alanloffler <alanmatiasloffler@gmail.com>
2026-01-31 20:32:23 -0300

    feat(users): update professional nested

--- src/users/dto/update-professional.dto.ts
+++ src/users/dto/update-professional.dto.ts
@@ -1,5 +1,23 @@
+import { IsObject, IsOptional, ValidateNested } from "class-validator";
 import { PartialType } from "@nestjs/mapped-types";
+import { Type } from "class-transformer";
 
-import { CreateProfessionalDto } from "@users/dto/create-professional.dto";
+import { CreateProfessionalProfileDto } from "@professional-profile/dto/create-professional-profile.dto";
+import { CreateUserDto } from "@users/dto/create-user.dto";
 
-export class UpdateProfessionalDto extends PartialType(CreateProfessionalDto) {}
+class UpdateUserDataDto extends PartialType(CreateUserDto) {}
+class UpdateProfileDataDto extends PartialType(CreateProfessionalProfileDto) {}
+
+export class UpdateProfessionalDto {
+  @ValidateNested()
+  @IsObject({ message: "Los datos del usuario deben ser un objeto" })
+  @IsOptional()
+  @Type(() => UpdateUserDataDto)
+  user?: UpdateUserDataDto;
+
+  @ValidateNested()
+  @IsObject({ message: "Los datos del perfil profesional deben ser un objeto" })
+  @IsOptional()
+  @Type(() => UpdateProfileDataDto)
+  profile?: UpdateProfileDataDto;
+}

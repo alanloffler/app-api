305e21c713770843dd15088ef6facd5831e5d523
alanloffler <alanmatiasloffler@gmail.com>
2026-01-31 19:40:05 -0300

    feat(users): update professional use case with queryRunner

---
+++ src/users/update-professional.use-case.ts
@@ -0,0 +1,40 @@
+import { DataSource } from "typeorm";
+import { Injectable } from "@nestjs/common";
+
+import { ApiResponse } from "@common/helpers/api-response.helper";
+import { ProfessionalProfileService } from "@professional-profile/professional-profile.service";
+import { UpdateProfessionalDto } from "@users/dto/update-professional.dto";
+import { UsersService } from "@users/users.service";
+
+@Injectable()
+export class UpdateProfessionalUseCase {
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly usersService: UsersService,
+    private readonly professionalProfileService: ProfessionalProfileService,
+  ) {}
+
+  async execute(userId: string, businessId: string, updateDto: UpdateProfessionalDto): Promise<ApiResponse<void>> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      if (updateDto.user) {
+        await this.usersService.update(userId, businessId, updateDto.user, queryRunner.manager);
+      }
+
+      if (updateDto.profile) {
+        await this.professionalProfileService.update(userId, businessId, updateDto.profile, queryRunner.manager);
+      }
+
+      await queryRunner.commitTransaction();
+      return ApiResponse.success("Profesional actualizado");
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+}

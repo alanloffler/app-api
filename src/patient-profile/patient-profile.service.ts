b239acb8f7b490b64ca10b94d6c1f99b5863076c
alanloffler <alanmatiasloffler@gmail.com>
2026-02-03 11:57:29 -0300

    feat(patient-profile): create service

--- src/patient-profile/patient-profile.service.ts
+++ src/patient-profile/patient-profile.service.ts
@@ -1,12 +1,25 @@
+import { EntityManager } from "typeorm";
 import { Injectable } from "@nestjs/common";
 
 import { CreatePatientProfileDto } from "@patient-profile/dto/create-patient-profile.dto";
+import { PatientProfile } from "@patient-profile/entities/patient-profile.entity";
 import { UpdatePatientProfileDto } from "@patient-profile/dto/update-patient-profile.dto";
 
 @Injectable()
 export class PatientProfileService {
-  create(createPatientProfileDto: CreatePatientProfileDto) {
-    return "This action adds a new patientProfile";
+  async create(
+    profileDto: CreatePatientProfileDto,
+    userId: string,
+    businessId: string,
+    manager: EntityManager,
+  ): Promise<PatientProfile> {
+    const profile = manager.create(PatientProfile, {
+      ...profileDto,
+      userId,
+      businessId,
+    });
+
+    return manager.save(profile);
   }
 
   findAll() {

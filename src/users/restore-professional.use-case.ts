0ed667bf72dc146080163c903871156928b63f20
alanloffler <alanmatiasloffler@gmail.com>
2026-02-03 20:29:53 -0300

    feat(professional-profile): restore service

---
+++ src/users/restore-professional.use-case.ts
@@ -0,0 +1,32 @@
+import { DataSource } from "typeorm";
+import { Injectable } from "@nestjs/common";
+
+import { ProfessionalProfileService } from "@professional-profile/professional-profile.service";
+import { UsersService } from "@users/users.service";
+
+@Injectable()
+export class RestoreProfessionalUseCase {
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly professionalProfileService: ProfessionalProfileService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async execute(userId: string, businessId: string) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await this.professionalProfileService.restore(userId, businessId, queryRunner.manager);
+      await this.usersService.restore(userId, businessId, queryRunner.manager);
+
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      queryRunner.release();
+    }
+  }
+}

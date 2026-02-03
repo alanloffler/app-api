9bc1de16b1543d946f3688539c64dc3974c58743
alanloffler <alanmatiasloffler@gmail.com>
2026-02-03 12:41:06 -0300

    remove(patient-profile): controller import

--- src/patient-profile/patient-profile.module.ts
+++ src/patient-profile/patient-profile.module.ts
@@ -2,14 +2,12 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { PatientProfile } from "@patient-profile/entities/patient-profile.entity";
-import { PatientProfileController } from "@patient-profile/patient-profile.controller";
 import { PatientProfileService } from "@patient-profile/patient-profile.service";
 import { Role } from "@roles/entities/role.entity";
 import { User } from "@users/entities/user.entity";
 
 @Module({
   imports: [TypeOrmModule.forFeature([PatientProfile, Role, User])],
-  controllers: [PatientProfileController],
   providers: [PatientProfileService],
 })
 export class PatientProfileModule {}

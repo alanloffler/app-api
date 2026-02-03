30a70723d394b42fd2fdde6bf20912606b78356b
alanloffler <alanmatiasloffler@gmail.com>
2026-02-03 17:50:38 -0300

    feat(users): soft remove professional use case

---
+++ src/users/soft-remove-professional.use-case.ts
@@ -0,0 +1,30 @@
+import { DataSource } from "typeorm";
+import { Injectable } from "@nestjs/common";
+
+import { ProfessionalProfileService } from "@professional-profile/professional-profile.service";
+import { UsersService } from "@users/users.service";
+
+@Injectable()
+export class SoftRemoveProfessionalUserCase {
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly professionalProfileService: ProfessionalProfileService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async execute(userId: string, businessId: string) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await this.professionalProfileService.softRemove(userId, businessId, queryRunner.manager);
+      await this.usersService.softRemove(userId, businessId, queryRunner.manager);
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      queryRunner.release();
+    }
+  }
+}

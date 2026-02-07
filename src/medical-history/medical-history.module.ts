dcfe6b5f97f9bf1e39e976ee21ba5ababc131ba9
alanloffler <alanmatiasloffler@gmail.com>
2026-02-07 19:52:01 -0300

    feat(medical-history): import Role & Permission for auth controllers

--- src/medical-history/medical-history.module.ts
+++ src/medical-history/medical-history.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { MedicalHistory } from "@medical-history/entities/medical-history.entity";
 import { MedicalHistoryController } from "@medical-history/medical-history.controller";
 import { MedicalHistoryService } from "@medical-history/medical-history.service";
+import { Permission } from "@permissions/entities/permission.entity";
+import { Role } from "@roles/entities/role.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MedicalHistory])],
+  imports: [TypeOrmModule.forFeature([MedicalHistory, Role, Permission])],
   controllers: [MedicalHistoryController],
   providers: [MedicalHistoryService],
 })
